16199c5cb8129d6044aeb44b23ff30c172f1b2c5
shahinkhalajestani <vatharishok@gmail.com>
2024-10-17 16:17:45 +0330

    project initials.

---
+++ src/main/java/com/shahinkhalajestani/customerproducttask/CustomerProductTaskApplication.java
@@ -0,0 +1,13 @@
+package com.shahinkhalajestani.customerproducttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProductTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProductTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shahinkhalajestani/customerproducttask/CustomerProductTaskApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shahinkhalajestani.customerproducttask;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerProductTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
